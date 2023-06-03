2e78f9ff600c22c42e1b86408efcbd53b4c1dc93
himanshusha673 <himanshusaha673@gmail.com>
2023-06-03 14:01:21 +0530

    added previous codes

--- lib/services/auth_methods.dart
+++ lib/services/auth_methods.dart
@@ -195,7 +195,6 @@ class AuthMethods {
             info.providerId == GoogleAuthProvider.GOOGLE_SIGN_IN_METHOD)) {
       GoogleSignIn googleSignIn = GoogleSignIn();
       await googleSignIn.disconnect();
-      await _auth.signOut();
     } else {
       await _auth.signOut();
     }
